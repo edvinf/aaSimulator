9033d9b8d16ce93e6173845edbe1bbdff0bb78ce
edvinf <edvin.fuglebakk@gmail.com>
2019-12-30 20:59:22 +0100

    minor cleanup

--- R/lhtr2.R
+++ R/lhtr2.R
@@ -1,37 +1,38 @@
 
 #' Makes unit table for LHTR 2.0
 #' @details
-#'  A value for baseAttack and baseDefence indicates that a unit can be taken as a casualty when attacking or defending respectively.
-#'
+#'  a value for baseAttack and baseDefence indicate that the unit can be taken as a casuality.
+#'  unit properties that need to be kept track of for battle calculation or reporting are represented as virtual units.
+#'  actions for specifying retreat or submerges are specified as represented as control units.
 #' @noRd
 #' @keywords internal
 makeUnitList <- function(){
   require(data.table)
-  infantry <- data.table(shortcut=as.character("inf"), name=as.character("Infantry"), cost=as.numeric(3), costOfLoss=as.numeric(3), baseAttack=as.integer(1), baseDefence=as.integer(2), move=as.integer(1), type=as.character("Land"), virtualUnit=F)
-  artillery <- data.table(shortcut=as.character("art"), name=as.character("Artillery"), cost=as.numeric(4), costOfLoss=as.numeric(4), baseAttack=as.integer(2), baseDefence=as.integer(2), move=as.integer(1), type=as.character("Land"), virtualUnit=F)
-  tank <- data.table(shortcut=as.character("arm"), name=as.character("Tank"), cost=as.numeric(5), costOfLoss=as.numeric(5), baseAttack=as.integer(3), baseDefence=as.integer(3), move=as.integer(2), type=as.character("Land"), virtualUnit=F)
-  antiaircraft <- data.table(shortcut=as.character("aa"), name=as.character("Anti-aircraft Gun"), cost=as.numeric(5), costOfLoss=as.numeric(5), baseAttack=as.integer(NA), baseDefence=as.integer(NA), move=as.integer(1), type=as.character("Land"), virtualUnit=F)
+  infantry <- data.table(shortcut=as.character("inf"), name=as.character("Infantry"), cost=as.numeric(3), baseAttack=as.integer(1), baseDefence=as.integer(2), move=as.integer(1), type=as.character("Land"), virtualUnit=F)
+  artillery <- data.table(shortcut=as.character("art"), name=as.character("Artillery"), cost=as.numeric(4), baseAttack=as.integer(2), baseDefence=as.integer(2), move=as.integer(1), type=as.character("Land"), virtualUnit=F)
+  tank <- data.table(shortcut=as.character("arm"), name=as.character("Tank"), cost=as.numeric(5), baseAttack=as.integer(3), baseDefence=as.integer(3), move=as.integer(2), type=as.character("Land"), virtualUnit=F)
+  antiaircraft <- data.table(shortcut=as.character("aa"), name=as.character("Anti-aircraft Gun"), cost=as.numeric(5), baseAttack=as.integer(NA), baseDefence=as.integer(NA), move=as.integer(1), type=as.character("Land"), virtualUnit=F)
 
   unitTable <- rbind(infantry, artillery, tank, antiaircraft)
 
-  fighter <- data.table(shortcut=as.character("ftr"), name=as.character("Fighter"), cost=as.numeric(10), costOfLoss=as.numeric(10), baseAttack=as.integer(3), baseDefence=as.integer(4), move=as.integer(4), type=as.character("Air"), virtualUnit=F)
-  bomber <- data.table(shortcut=as.character("bmb"), name=as.character("Bomber"), cost=as.numeric(15), costOfLoss=as.numeric(15), baseAttack=as.integer(4), baseDefence=as.integer(1), move=as.integer(6), type=as.character("Air"), virtualUnit=F)
+  fighter <- data.table(shortcut=as.character("ftr"), name=as.character("Fighter"), cost=as.numeric(10), baseAttack=as.integer(3), baseDefence=as.integer(4), move=as.integer(4), type=as.character("Air"), virtualUnit=F)
+  bomber <- data.table(shortcut=as.character("bmb"), name=as.character("Bomber"), cost=as.numeric(15), baseAttack=as.integer(4), baseDefence=as.integer(1), move=as.integer(6), type=as.character("Air"), virtualUnit=F)
 
   unitTable <- rbind(unitTable, fighter, bomber)
 
-  battleship <- data.table(shortcut=as.character("bb"), name=as.character("Battleship"), cost=as.numeric(24), costOfLoss=as.numeric(24), baseAttack=as.integer(4), baseDefence=as.integer(4), move=as.integer(2),type=as.character("Sea"), virtualUnit=F)
-  battleshipbombardment <- data.table(shortcut=as.character("BBomb"), name=as.character("Battleship offshore bombardment"), cost=as.numeric(NA), costOfLoss=as.numeric(NA), baseAttack=as.integer(NA), baseDefence=as.integer(NA), move=as.integer(2), type=as.character("Land"), virtualUnit=T)
-  battleshipfirst <- data.table(shortcut=as.character("BBx"), name=as.character("Battleship first hit"), cost=as.numeric(NA), costOfLoss=as.numeric(NA), baseAttack=as.integer(0), baseDefence=as.integer(0), move=as.integer(NA), type=as.character("Sea"), virtualUnit=T)
-  destroyer <- data.table(shortcut=as.character("dd"), name=as.character("Destroyer"), cost=as.numeric(12), costOfLoss=as.numeric(12), baseAttack=as.integer(3), baseDefence=as.integer(3), move=as.integer(2), type=as.character("Sea"), virtualUnit=F)
-  carrier <- data.table(shortcut=as.character("ac"), name=as.character("Aircraft Carrier"), cost=as.numeric(16), costOfLoss=as.numeric(16), baseAttack=as.integer(1), baseDefence=as.integer(3), move=as.integer(2), type=as.character("Sea"), virtualUnit=F)
-  submarine <- data.table(shortcut=as.character("sub"), name=as.character("Submarine"), cost=as.numeric(8), costOfLoss=as.numeric(8), baseAttack=as.integer(2), baseDefence=as.integer(2), move=as.integer(2), type=as.character("Sea"), virtualUnit=F)
-  submerged <- data.table(shortcut=as.character("subm"), name=as.character("Submerged Submarine"), cost=as.numeric(NA), costOfLoss=as.numeric(NA), baseAttack=as.integer(NA), baseDefence=as.integer(NA), move=as.integer(NA), type=as.character("Sea"), virtualUnit=T)
-  transport <- data.table(shortcut=as.character("trn"), name=as.character("Transport"), cost=as.numeric(8), costOfLoss=as.numeric(8), baseAttack=as.integer(0), baseDefence=as.integer(1), move=as.integer(2), type=as.character("Sea"), virtualUnit=F)
+  battleship <- data.table(shortcut=as.character("bb"), name=as.character("Battleship"), cost=as.numeric(24), baseAttack=as.integer(4), baseDefence=as.integer(4), move=as.integer(2),type=as.character("Sea"), virtualUnit=F)
+  battleshipbombardment <- data.table(shortcut=as.character("BBomb"), name=as.character("Battleship offshore bombardment"), cost=as.numeric(NA), baseAttack=as.integer(NA), baseDefence=as.integer(NA), move=as.integer(2), type=as.character("Land"), virtualUnit=T)
+  battleshipfirst <- data.table(shortcut=as.character("BBx"), name=as.character("Battleship first hit"), cost=as.numeric(NA), baseAttack=as.integer(0), baseDefence=as.integer(0), move=as.integer(NA), type=as.character("Sea"), virtualUnit=T)
+  destroyer <- data.table(shortcut=as.character("dd"), name=as.character("Destroyer"), cost=as.numeric(12), baseAttack=as.integer(3), baseDefence=as.integer(3), move=as.integer(2), type=as.character("Sea"), virtualUnit=F)
+  carrier <- data.table(shortcut=as.character("ac"), name=as.character("Aircraft Carrier"), cost=as.numeric(16), baseAttack=as.integer(1), baseDefence=as.integer(3), move=as.integer(2), type=as.character("Sea"), virtualUnit=F)
+  submarine <- data.table(shortcut=as.character("sub"), name=as.character("Submarine"), cost=as.numeric(8), baseAttack=as.integer(2), baseDefence=as.integer(2), move=as.integer(2), type=as.character("Sea"), virtualUnit=F)
+  submerged <- data.table(shortcut=as.character("subm"), name=as.character("Submerged Submarine"), cost=as.numeric(NA), baseAttack=as.integer(NA), baseDefence=as.integer(NA), move=as.integer(NA), type=as.character("Sea"), virtualUnit=T)
+  transport <- data.table(shortcut=as.character("trn"), name=as.character("Transport"), cost=as.numeric(8),baseAttack=as.integer(0), baseDefence=as.integer(1), move=as.integer(2), type=as.character("Sea"), virtualUnit=F)
 
   unitTable <- rbind(unitTable, battleship, battleshipbombardment, battleshipfirst, destroyer, carrier, submarine, submerged, transport)
 
-  retreat <- data.table(shortcut=as.character("RET"), name=as.character("Retreat action"), cost=as.numeric(NA), costOfLoss=as.numeric(NA), baseAttack=as.integer(NA), baseDefence=as.integer(NA), move=as.integer(NA),type=as.character("Control"), virtualUnit=T)
-  submerge <- data.table(shortcut=as.character("SUBM"), name=as.character("Submerge action"), cost=as.numeric(NA), costOfLoss=as.numeric(NA), baseAttack=as.integer(NA), baseDefence=as.integer(NA), move=as.integer(NA),type=as.character("Control"), virtualUnit=T)
+  retreat <- data.table(shortcut=as.character("RET"), name=as.character("Retreat action"), cost=as.numeric(NA), baseAttack=as.integer(NA), baseDefence=as.integer(NA), move=as.integer(NA),type=as.character("Control"), virtualUnit=T)
+  submerge <- data.table(shortcut=as.character("SUBM"), name=as.character("Submerge action"), cost=as.numeric(NA), baseAttack=as.integer(NA), baseDefence=as.integer(NA), move=as.integer(NA),type=as.character("Control"), virtualUnit=T)
 
   unitTable <- rbind(unitTable, retreat, submerge)
 
@@ -213,18 +214,24 @@ play_LHTR_battle_round <- function(oolAttacker, oolDefender, roundnr, submergeAt
 #' @description Plays one round of battle following Larry Harris Tournament Ruls for Axis and Allies Revised edition (LHTR 2.0)
 #' @details
 #'  In addition to the standard options, the attacker and defender OOL supports the directives:
-#'  \item{+sumberge}{Submarines submerge at first opportunity (after any round of combat with no enemy destroyer present)}
+#'  \describe{
+#'   \item{+sumberge}{Submarines submerge at first opportunity (after any round of combat with no enemy destroyer present)}
+#'   }
 #'  In addition to the standard options, the attacker OOL supports the directives:
-#'  \item{+roundretreat <n>}{Attacker will retreat after round n.}
-#'  \item{+ret}{Attacker will retreat when all units preceeding this directive is lost.}
+#'  \describe{
+#'   \item{+roundretreat <n>}{Attacker will retreat after round n.}
+#'   \item{+ret}{Attacker will retreat when all units preceeding this directive is lost.}
+#'  }
 #' @param oolAttacker character() order of loss for attacker
 #' @param oolDefender character() order of loss for defender
 #' @param retreat integer() round number after which attacker should retreat, if NULL, round number will not trigger retreat
 #' @return list() with members
+#' \describe{
 #'  \item{attackerLoss}{integer() attacker IPC loss}
 #'  \item{defenderLoss}{integer() defender IPC loss}
 #'  \item{unitsAttacker}{data.table with columns for units for the attacker after battle}
 #'  \item{unitsDefender}{data.table with columns for units for the defender after battle}
+#'  }
 #' @export
 play_LHTR_battle <- function(oolAttacker, oolDefender, retreat=NULL){
 
