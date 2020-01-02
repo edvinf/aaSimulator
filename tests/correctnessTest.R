79b9e36f7eac93144d8b4892a9f9aefaf52f390c
edvinf <edvin.fuglebakk@gmail.com>
2020-01-02 12:26:09 +0100

    Icreased replicates

--- tests/correctnessTest.R
+++ tests/correctnessTest.R
@@ -20,7 +20,7 @@ checkAgainstTripleA <- function(stats, wins, draw, loose, rounds, TUVswing, trip
 #
 # Test some examples against TripleA calculator
 #
-s <- simulateBattles(c("inf", "art", "arm"), c("inf", "inf", "inf"), iterations = 2000, replications = 3)
+s <- simulateBattles(c("inf", "art", "arm"), c("inf", "inf", "inf"), iterations = 2000, replications = 5)
 stats <- calculateStats(s)
 checkAgainstTripleA(stats, .58, .06, .35, 2.78, -0.06, 2000, "inf art arm vs 3 inf")
 
@@ -36,10 +36,10 @@ checkAgainstTripleA(stats, .58, .06, .35, 2.78, -0.06, 2000, "inf art arm vs 3 i
 #checkAgainstTripleA(stats, .85, .03, .12, 2.36, .15, 2000, "inf art arm ftr vs 3 inf AA")
 
 
-s <- simulateBattles(c("inf", "art", "arm", "ftr"), c("inf", "inf", "inf"), iterations = 2000, replications = 3)
+s <- simulateBattles(c("inf", "art", "arm", "ftr"), c("inf", "inf", "inf"), iterations = 2000, replications = 5)
 stats <- calculateStats(s)
 checkAgainstTripleA(stats, .90, .02, .07, 2.29, 2.06, 2000, "inf art arm ftr vs 3 inf")
 
-s <- simulateBattles(c("ftr", "art", "arm", "inf"), c("inf", "inf", "inf"), iterations = 2000, replications = 3)
+s <- simulateBattles(c("ftr", "art", "arm", "inf"), c("inf", "inf", "inf"), iterations = 2000, replications = 5)
 stats <- calculateStats(s)
 checkAgainstTripleA(stats, .85, .01, .14, 2.51, -3.85, 2000, "ftr art arm inf vs 3 inf")
