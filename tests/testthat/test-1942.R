6a5cb890388dfd0a6e9e54c5d91aebedba7da4d9
edvinf <edvin.fuglebakk@gmail.com>
2020-05-28 00:58:11 +0200

    outline for tests

---
+++ tests/testthat/test-1942.R
@@ -0,0 +1,9 @@
+context("1942 second edition")
+
+# test against https://www.aatoolkit.com/conflict
+
+# test fun combinations of air and sub with and without dd
+
+# test aa guns
+
+# test check on transports in OOL
